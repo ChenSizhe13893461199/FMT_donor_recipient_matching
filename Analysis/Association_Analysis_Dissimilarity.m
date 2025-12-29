6bf0557edd6a24c7783e185d8598ef617fdbf71b
Sizhe Chen <129570497+ChenSizhe13893461199@users.noreply.github.com>
2025-12-29 16:34:01 +0800

    Update Association_Analysis_Dissimilarity.m

--- Analysis/Association_Analysis_Dissimilarity.m
+++ Analysis/Association_Analysis_Dissimilarity.m
@@ -1,7 +1,12 @@
 %Association Analysis (Profile Dissimilarity vs Increased Profile Similarity)
 data = readtable('Source Data File.xlsx');
-increased = data.Overall_Increased_Similarity; % Increased Similarity
-dissimilarity = data.Overall_Microbiome_Dissimilarity; % Dissimilarity between donor pre-FMT recipient
+increased = data.Overall_Increased_Similarity; % Increased Similarity in Microbiome Profiles
+%increased = data.Pathway_increased_similarity; % Increased Similarity in Pathway Profiles
+%increased = data.KO_increased; % Increased Similarity in KO Profiles
+
+dissimilarity = data.Overall_Microbiome_Dissimilarity; % Dissimilarity between donor pre-FMT recipient in microbiome profile
+%dissimilarity = data.Pathway_dissimilarity; % Dissimilarity between donor pre-FMT recipient in Pathway profile
+%dissimilarity = data.KO_dissimilarity; % Dissimilarity between donor pre-FMT recipient in KO profile
 
 valid_idx = ~isnan(increased) & ~isnan(dissimilarity);
 increased = increased(valid_idx);
