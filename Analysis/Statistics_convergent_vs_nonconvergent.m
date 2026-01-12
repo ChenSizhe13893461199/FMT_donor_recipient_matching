fb8427ba1d4df98bc2f381487cd5e1d595440d45
Sizhe Chen <129570497+ChenSizhe13893461199@users.noreply.github.com>
2026-01-12 19:43:28 +0800

    Update Statistics_convergent_vs_nonconvergent.m

--- Analysis/Statistics_convergent_vs_nonconvergent.m
+++ Analysis/Statistics_convergent_vs_nonconvergent.m
@@ -14,9 +14,10 @@ data.Gender = categorical(data.Gender); % Categorical Variable
 model = fitlm(data, ...
     sprintf('%s ~ Group + DiseasesClass+Gender+Age+antibiotics', data.Properties.VariableNames{29}), ...
     'CategoricalVars', {'Group', 'DiseasesClass','Gender','antibiotics'}); 
-    
-%Here 29 represents the 29th column (pre-FMT recipient Shannon); Please replace it with other numbers if further analysis for other indexes are needed
+    %Here 29 represents the 29th column (pre-FMT recipient Shannon); Please replace it with other numbers if further analysis for other indexes are needed
+
 disp(model);
+
 group_coef_name = model.CoefficientNames{contains(model.CoefficientNames, 'Group')};
 p_value = model.Coefficients{group_coef_name, 'pValue'};
 p_value
