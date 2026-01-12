18936a9d655290b9eb653b9d26bfb77fabe5d248
Sizhe Chen <129570497+ChenSizhe13893461199@users.noreply.github.com>
2026-01-12 19:35:44 +0800

    Update Statistics.m

--- Analysis/Statistics.m
+++ Analysis/Statistics.m
@@ -1,4 +1,4 @@
-% ANOVA-based Multiple Linear Regression Codes with multi-confounders adjustment
+% ANOVA-based Multiple Linear Regression Codes with multi-confounders adjustment (This script can only be used for comparing responders vs non-responders)
 
 data = readtable('Source Data File.xlsx');
 data = data(data.category_response_1_or_non_response_0_or_unknown_2 == 0 | data.category_response_1_or_non_response_0_or_unknown_2 == 1, :); % analyzing Responders & Non-Responders data
@@ -17,8 +17,6 @@ model = fitlm(data, ...
     'CategoricalVars', {'Group', 'DiseasesClass','Gender','antibiotics'}); 
     
 %Here 5 represents the 5th column (bacteria profile), for fungi, virus, and archaea as well as overall microbiomne, please replace it with number 6, 7, 8, and 9, respectively
-%This script can also be used for analyzing multiple indexes used in our study (see Source Data File.xlsx), and please replace the mentioned position with the
-%corresponding number (e.g. the 14th column represents Overall_Microbiome_Dissimilarity, and it can be adjusted by the script to avoid influences from the confounding variables) to conduct analysis, respectively.
 disp(model);
 group_coef_name = model.CoefficientNames{contains(model.CoefficientNames, 'Group')};
 p_value = model.Coefficients{group_coef_name, 'pValue'};
